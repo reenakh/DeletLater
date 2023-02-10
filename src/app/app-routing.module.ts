34db20fc68a7a1b67d50ca4bb3d5841c401eed65
reenakh <111008141+reenakh@users.noreply.github.com>
2023-02-11 04:49:24 +0530

    FirstCommit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
+import { MatFormField } from '@angular/material';
 import { RouterModule, Routes } from '@angular/router';
+import { WasteComponent } from './waste/waste.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'waste',component:WasteComponent},
+  {path:'login', loadChildren:()=>import('./shared/shared.module').then(m=>m.SharedModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
+import {MatTableModule} from '@angular/material/table';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WasteComponent } from './waste/waste.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from './shared/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WasteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatTableModule,
+    NgbModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
